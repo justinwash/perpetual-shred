39fff0018ea6958d9cf4c448cdcb88bcb998aceb
Wash <washj@stifel.com>
2018-10-11 13:29:53 -0500

    connection string configuration

--- frontend/src/app/components/create/create.component.ts
+++ frontend/src/app/components/create/create.component.ts
@@ -4,31 +4,31 @@ import { Router } from '@angular/router';
 import { VidService } from '../../services/vid.service';
 
 @Component({
-	selector: 'app-create',
-	templateUrl: './create.component.html',
-	styleUrls: ['./create.component.css']
+  selector: 'app-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
 
-	createForm: FormGroup;
+  createForm: FormGroup;
 
-	constructor(private vidService: VidService, private fb: FormBuilder, private router: Router) {
-		this.createForm = this.fb.group({
-			title: ['', Validators.required],
-			description: '',
-			origin: '',
-			releaseDate: '',
-			url: ''
-		});
-	}
+  constructor(private vidService: VidService, private fb: FormBuilder, private router: Router) {
+    this.createForm = this.fb.group({
+      title: ['', Validators.required],
+      description: '',
+      origin: '',
+      releaseDate: '',
+      url: ''
+    });
+  }
 
-	addVid(title, description, origin, releaseDate, url) {
-		this.vidService.addVid(title, description, origin, releaseDate, url).subscribe(() => {
-			this.router.navigate(['/list']);
-		});
-	}
+  addVid(title, description, origin, releaseDate, url) {
+    this.vidService.addVid(title, description, origin, releaseDate, url).subscribe(() => {
+      this.router.navigate(['/admin']);
+    });
+  }
 
-	ngOnInit() {
-	}
+  ngOnInit() {
+  }
 
 }
