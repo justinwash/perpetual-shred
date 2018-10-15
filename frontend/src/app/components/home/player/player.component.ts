de1294d0f5ba27b931b6348822879cfac7dc01cb
Trifectuh <justin.wash@pm.me>
2018-10-15 01:51:28 -0500

    youtube event handler

--- frontend/src/app/components/home/player/player.component.ts
+++ frontend/src/app/components/home/player/player.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Vid } from '../../../models/vid.model';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { VidService } from '../../../services/vid.service';
+import { YouTubeService } from '../../../services/youtube.service';
 
 @Component({
   selector: 'app-player',
@@ -9,20 +11,35 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 })
 export class PlayerComponent implements OnInit {
   @Input() vid: Vid;
+  player: any;
   playerType: string;
   safeVidUrl: SafeResourceUrl;
-  constructor(private sanitizer: DomSanitizer) { }
+  constructor(private sanitizer: DomSanitizer,
+              private vidService: VidService,
+              private ytService: YouTubeService) { }
 
-  ngOnInit() {
-    this.playerType = this.detectPlayerType();
-    this.safeVidUrl = this.buildSafeVidUrl(this.vid.url);
+  ngAfterViewInit() {
   }
 
-  detectPlayerType(): string {
-    return 'youtube';
+  ngOnInit() {
+    this.playerType = this.vidService.getPlayerType(this.vid.url);
+    this.safeVidUrl = this.buildSafeVidUrl(this.vid.url);
+    this.createYouTubePlayer(this.vid);
   }
 
   buildSafeVidUrl(url: string) {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url + '?autoplay=1');
+    if(this.playerType === 'youtube')
+      return this.sanitizer.bypassSecurityTrustResourceUrl(url + '?autoplay=1&enablejsapi=1');
+  }
+
+  createYouTubePlayer(vid: Vid){
+    this.ytService.initializeYoutubeApi();
+    (<any>window).onYouTubeIframeAPIReady = () => {
+      this.player = new (<any>window).YT.Player('player');
+      console.log(this.player);
+    }
+  }
+  stopVid() {
+    this.player.stopVideo();
   }
 }
