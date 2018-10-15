a9d13089b5b9c069c80d56226a5aabed5199a4bf
Wash <washj@stifel.com>
2018-10-15 16:43:20 -0500

    remove unneccessary usings

--- backend/login.js
+++ backend/login.js
@@ -1,5 +1,5 @@
 // PerpetualShred connection string
-export const psConnectionString = 'mongodb+srv://dev:dev@perpetualshred-rdiyi.gcp.mongodb.net/perpetualshred?retryWrites=true';
+export const psConnectionString = '';
 
 // ShredUsers Connection string (not yet used)
 export const suConnectionString = '';
