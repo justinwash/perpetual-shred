71b7eba6530380f4c915ca1065108290a4cd31aa
Trifectuh <justin.wash@pm.me>
2018-10-15 22:21:51 -0500

    basic crawler

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import { psConnectionString } from './login';
+import crawler from './crawler';
 
 import Vid from './models/Vid';
 
@@ -17,66 +18,71 @@ mongoose.connect(psConnectionString);
 const connection = mongoose.connection;
 
 connection.once('open', () => {
-	console.log('MongoDB database connection established successfully!');
+    console.log('MongoDB database connection established successfully!');
 });
 
 router.route('/vids').get((req, res) => {
-	Vid.find((err, vids) => {
-		if (err)
-			console.log(err);
-		else
-			res.json(vids);
-	});
+    Vid.find((err, vids) => {
+        if (err)
+            console.log(err);
+        else
+            res.json(vids);
+    });
 });
 
 router.route('/vids/:id').get((req, res) => {
-	Vid.findById(req.params.id, (err, vid) => {
-		if (err)
-			console.log(err);
-		else
-			res.json(vid);
-	});
+    Vid.findById(req.params.id, (err, vid) => {
+        if (err)
+            console.log(err);
+        else
+            res.json(vid);
+    });
 });
 
 router.route('/vids/add').post((req, res) => {
-	let vid = new Vid(req.body);
-	vid.save()
-		.then(vid => {
-			res.status(200).json({ 'vid': 'Added successfully' });
-		})
-		.catch(err => {
-			res.status(400).send('Failed to create new record');
-		});
+    let vid = new Vid(req.body);
+    vid.save()
+        .then(vid => {
+            res.status(200).json({ 'vid': 'Added successfully' });
+        })
+        .catch(err => {
+            res.status(400).send('Failed to create new record');
+        });
 });
 
 router.route('/vids/update/:id').post((req, res) => {
-	Vid.findById(req.params.id, (err, vid) => {
-		if (!vid)
-			return next(new Error('Could not load document'));
-		else {
-			vid.title = req.body.title;
-			vid.description = req.body.description;
-			vid.origin = req.body.origin;
-			vid.releaseDate = req.body.releaseDate;
-			vid.url = req.body.url;
+    Vid.findById(req.params.id, (err, vid) => {
+        if (!vid)
+            return next(new Error('Could not load document'));
+        else {
+            vid.title = req.body.title;
+            vid.description = req.body.description;
+            vid.origin = req.body.origin;
+            vid.releaseDate = req.body.releaseDate;
+            vid.url = req.body.url;
 
-			vid.save().then(vid => {
-				res.json('Update done');
-			}).catch(err => {
-				res.status(400).send('Update failed');
-			});
-		}
-	});
+            vid.save().then(vid => {
+                res.json('Update done');
+            }).catch(err => {
+                res.status(400).send('Update failed');
+            });
+        }
+    });
 });
 
 router.route('/vids/delete/:id').get((req, res) => {
-	Vid.findByIdAndRemove({ _id: req.params.id }, (err, vid) => {
-		if (err)
-			res.json(err);
-		else
-			res.json('Remove successfully');
-	})
-})
+    Vid.findByIdAndRemove({ _id: req.params.id }, (err, vid) => {
+        if (err)
+            res.json(err);
+        else
+            res.json('Remove successfully');
+    });
+});
+
+router.route('/scrape').get((req, res) => {
+    crawler.crawl();
+    res.json('Crawler started')
+});
 
 app.use('/', router);
 
